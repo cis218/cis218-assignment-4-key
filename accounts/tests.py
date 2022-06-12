a3c26e4464dc1172d2e7a42a315d05284be63c76
David Barnes <dbarnes@kvcc.edu>
2022-06-12 15:40:03 -0400

    Add ability for users to be able to update their own account info.

--- accounts/tests.py
+++ accounts/tests.py
@@ -32,3 +32,49 @@ class SignupPageTests(TestCase):
         self.assertEqual(get_user_model().objects.all().count(), 1)
         self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
         self.assertEqual(get_user_model().objects.all()[0].email, "testuser@email.com")
+
+
+class ProfilePageTests(TestCase):
+    """Profile Page Tests"""
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+        )
+
+    def test_url_exists_at_correct_location_profileview(self):
+        """Test url exists at correct location profileview"""
+        self.client.force_login(self.user)
+        response = self.client.get(f"/accounts/profile/{self.user.pk}/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_view_name(self):
+        """Test profile view name"""
+        self.client.force_login(self.user)
+        response = self.client.get(reverse("profile", kwargs={"pk": self.user.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/profile.html")
+
+    def test_profile_form(self):
+        """Test profile form"""
+        self.client.force_login(self.user)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        response = self.client.post(
+            reverse("profile", kwargs={"pk": self.user.pk}),
+            {
+                "username": "testuser",
+                "email": "test@email.com",
+                "first_name": "testy",
+                "last_name": "mctester",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(self.user.username, "testuser")
+        self.assertEqual(self.user.email, "test@email.com")
+        self.assertEqual(self.user.first_name, "testy")
+        self.assertEqual(self.user.last_name, "mctester")
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+
+from .models import CustomUser
 
 from .forms import CustomUserCreationForm
 
@@ -10,3 +14,18 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    """Profile View"""
+
+    model = CustomUser
+    success_url = reverse_lazy("twit_list")
+    template_name = "registration/profile.html"
+    fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "date_of_birth",
+    )
