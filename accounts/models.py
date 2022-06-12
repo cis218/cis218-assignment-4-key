66213275f96c94cee1ebebd5e9ca776b1dafaeb2
David Barnes <dbarnes@kvcc.edu>
2022-06-12 15:40:03 -0400

    Add ability for users to be able to update their own account info.

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class CustomUser(AbstractUser):
     """Custom User Model"""
 
     date_of_birth = models.DateField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse("profile", kwargs={"pk": self.pk})
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, ProfileView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/<int:pk>/", ProfileView.as_view(), name="profile"),
 ]
