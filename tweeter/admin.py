e8ef5609901e0bad14bd36039de4f851c428da3b
David Barnes <dbarnes@kvcc.edu>
2022-06-13 19:54:04 -0400

    Add ability to add and see comments on a twit.

--- tweeter/admin.py
+++ tweeter/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Twit
+from .models import Twit, Comment
 
 
-admin.site.register(Twit)
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class TwitAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Twit, TwitAdmin)
+admin.site.register(Comment)
--- tweeter/models.py
+++ tweeter/models.py
@@ -29,3 +29,30 @@ class Twit(models.Model):
 
     class Meta:
         ordering = ("-created_at",)
+
+
+class Comment(models.Model):
+    """A single Comment on a Twit"""
+
+    twit = models.ForeignKey(
+        Twit,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    text = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
+    def get_absolute_url(self):
+        return reverse("twit_list")
+
+    class Meta:
+        ordering = ("created_at",)
